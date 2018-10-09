3272a85b6f995522b7a60c42497e382f36f642db
Rob Gaston <rgaston@fargeo.com>
2018-10-08 19:42:21 -0700

    updating map

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ let map;
 
 async function init() {
     const custom = await import("./custom-style.json");
+
+    const neighborhoods = await import("../data/output.json");
+
     const style = map.getStyle();
 
     style.sources = {
@@ -15,8 +18,76 @@ async function init() {
     };
     style.layers.push(...custom.layers);
     map.setStyle(style);
+
+    map.getSource('neighborhoods').setData(neighborhoods);
+
+
+    initLegend();
+    initPopup();
+}
+
+let hovered;
+const popup = document.querySelector('#popup');
+function initPopup() {
+    const nameSpan = document.querySelector('#name');
+    const countSpan = document.querySelector('#count');
+
+    map.on('mousemove', 'neighborhoods', function(e) {
+        clearHover();
+        if (e.features.length > 0) {
+            hovered = e.features[0];
+            map.setFeatureState(hovered, {
+                'hover': true
+            });
+            popup.style.display = 'block';
+
+            nameSpan.textContent = hovered.properties.name;
+            countSpan.textContent = hovered.properties.count;
+        }
+    });
+
+    map.on('mouseleave', 'neighborhoods', clearHover)
+}
+
+function clearHover() {
+    if (hovered) {
+        map.setFeatureState(hovered, {
+            'hover': false
+        });
+    }
+    popup.style.display = 'none';
+    hovered = null;
+}
+
+function initLegend() {
+    const legend = document.querySelector('#legend');
+    const template = document.querySelector('#legend-entry');
+
+    const colors = map.getPaintProperty(
+        'neighborhoods',
+        'fill-extrusion-color'
+    ).stops;
+
+    colors.forEach(function(color, i) {
+        const entry = document.importNode(template.content, true);
+        const colorSpan = entry.querySelector('.color');
+        const valueSpan = entry.querySelector('.value');
+
+        colorSpan.style.backgroundColor = color[1]
+
+        if (colors.length === i+1) {
+            valueSpan.textContent = `>=${color[0]}`;
+        } else {
+            valueSpan.textContent = `${color[0]}-${colors[i+1][0]-1}`;
+        }
+
+        legend.appendChild(entry);
+    });
 }
 
 mapboxgl.accessToken = settings.accessToken;
+
+settings.customAttribution = document.querySelector('#attribution').innerHTML;
+
 map = new mapboxgl.Map(settings);
 map.on("load", init);
--- src/process.mjs
+++ src/process.mjs
@@ -1,1 +1,28 @@
-console.log("success. 👍");
+import fs from "fs";
+import turf from "@turf/turf";
+import neighborhoods from "../data/neighborhoods.json";
+import historicPlaces from "../data/historic-places.json";
+
+historicPlaces.features.forEach(function(feature) {
+    feature.properties = {
+        count: 1
+    };
+});
+
+let output = turf.collect(neighborhoods, historicPlaces, 'count', 'count');
+
+output.features = output.features.filter(function(feature, i) {
+    feature.id = i;
+    feature.properties.count = feature.properties.count.reduce(function(value, count) {
+        return count + value;
+    }, 0);
+    return feature.properties.count > 0;
+});
+
+output = JSON.stringify(output, null, "\t");
+
+fs.writeFile("../data/output.json", output, function(err) {
+    if (err) throw err;
+
+    console.log("success. 👍");
+});
